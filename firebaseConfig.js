8407fd20f5e7251f87eda0f93f209919f2854575
VAISHNU R <161015641+Vaishnu1@users.noreply.github.com>
2025-08-02 12:12:13 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,13 +5,13 @@ import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 // Your Firebase configuration object
 const firebaseConfig = {
-  apiKey: "AIzaSyDsJfrFG2_wRIP8MOT_P7R3Ab_NSz0LZUk",
-  authDomain: "to-do-8ee8f.firebaseapp.com",
-  projectId: "to-do-8ee8f",
-  storageBucket: "to-do-8ee8f.firebasestorage.app",
-  messagingSenderId: "877112973840",
-  appId: "1:877112973840:web:1399beff0a75e09cd5c895",
-  measurementId: "G-T0VVZM8TBC"
+  apiKey: "AIzaSyA8BykUfTs3iYtBf5k-LjcF5D0-1beeULY",
+  authDomain: "react-todo-7e88a.firebaseapp.com",
+  projectId: "react-todo-7e88a",
+  storageBucket: "react-todo-7e88a.firebasestorage.app",
+  messagingSenderId: "655556786906",
+  appId: "1:655556786906:web:2decc5571139f9691143e3",
+  measurementId: "G-GR9XWYKGZG"
 };
 
 // Initialize Firebase
